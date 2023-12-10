d61c4fb7ebdecea064a2b814a47863ca3bd4454c
TK <developer.aungkaungkhant@gmail.com>
2023-12-10 19:12:36 +0630

    refactor: homework

---
+++ src/components/Shop/ProductCard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { useAppStore } from "../../store/useAppStore"
+
+const ProductCard = ({ card }) => {
+  const { addToCart } = useAppStore()
+  const [quantity, setQuantity] = useState(1)
+
+  const doAddToCart = () => addToCart(card.id, quantity)
+
+  return (
+    <div className="inline-block m-10 text-center border shadow-2xl Item__Card border-black-200">
+      <div className="w-[200px] h-auto">
+        <img
+          src={card.imageUrl}
+          alt={card.imageAlt}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <p className="font-serif italic">{card.description}</p>
+      <p className="font-medium">US {card.price}</p>
+      <div className="text-lg font-md">
+        <label>Quantity</label>
+        <input
+          value={quantity}
+          onChange={(e) => setQuantity(parseInt(e.target.value))}
+          type="number"
+          min="1"
+          max="10"
+          className="w-10 m-2 border"
+        />
+      </div>
+      <button
+        type="button"
+        onClick={doAddToCart}
+        className="px-2 my-2 ml-2 text-lg uppercase bg-red-400 border border-black rounded-full"
+      >
+        Add to Cart
+      </button>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,6 +1,7 @@
 import { TfiShoppingCart } from "react-icons/tfi"
 import { useAppStore } from "../../store/useAppStore"
-import { useState } from "react"
+import ProductCard from "./ProductCard"
+import { useEffect, useState } from "react"
 // mock data
 const cards = [
   {
@@ -39,40 +40,29 @@ const cards = [
 ]
 
 const ShopPage = () => {
-  const { cart, addToCart, clearCart } = useAppStore()
+  const { cart, clearCart } = useAppStore()
+  const [cartCount, setCartCount] = useState(0)
 
-  console.log("this is cart value", cart)
+  // update count whenever cart is updated
+  useEffect(() => {
+    let result = 0
+    cart.forEach((c) => (result += c.quantity))
+    console.log("result", result)
 
-  const [amount, setAmount] = useState({})
-
-  const handleAmount = (cardId, quantity) => {
-    //switch string into number
-    const quantityValue = parseInt(quantity, 10)
-
-    setAmount((prev) => ({
-      ...prev,
-      [cardId]: quantityValue,
-    }))
-  }
-
-  const submitHandler = (e, cardId) => {
-    console.log("card Id =", cardId)
-    e.preventDefault()
-    addToCart(cardId, amount[cardId])
-    setAmount("")
-  }
+    setCartCount(result)
+  }, [cart])
 
   return (
     <div>
-      <div className="flex justify-end text-center p-5">
-        <div className="flex relative">
+      <div className="flex justify-end p-5 text-center">
+        <div className="relative flex">
           <div className="flex cursor-pointer" onClick={clearCart}>
             <TfiShoppingCart size={30} />
             <p className="text-xl">My Cart</p>
           </div>
 
           <div className="absolute  top-0 left-0 -translate-x-[50%] -translate-y-[50%] flex justify-center w-[20px] h-[20px] bg-red-500 text-sm items-center text-white rounded-full">
-            {cart.length >= 10 ? "9+" : cart.length}
+            {cartCount >= 10 ? "9+" : cartCount}
           </div>
         </div>
       </div>
@@ -81,45 +71,11 @@ const ShopPage = () => {
       </div>
       <div>
         {cards.map((card, i) => (
-          <div
-            className="Item__Card inline-block text-center shadow-2xl border border-black-200 m-10"
-            key={i}
-          >
-            <div className="w-[200px] h-auto">
-              <img
-                src={card.imageUrl}
-                alt={card.imageAlt}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <p className="font-serif italic">{card.description}</p>
-            <p className="font-medium">US {card.price}</p>
-            <form action="" onSubmit={(e) => submitHandler(e, card.id)}>
-              <div className="text-lg font-md">
-                <label>Quantity</label>
-                <input
-                  value={amount[card.id] || ""}
-                  onChange={(e) => handleAmount(card.id, e.target.value)}
-                  type="number"
-                  min="1"
-                  max="10"
-                  step="1"
-                  className="w-10 m-2 border"
-                />
-              </div>
-              <button
-                type="submit"
-                className="px-2 ml-2 my-2 uppercase text-lg bg-red-400 border border-black rounded-full"
-              >
-                Add to Cart
-              </button>
-            </form>
-          </div>
+          <ProductCard key={i} card={card} />
         ))}
       </div>
     </div>
   )
 }
-// hello
 
 export default ShopPage
